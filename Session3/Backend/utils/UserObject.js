6c3b1eae1689e93b4f871678d92fdeb799455ad2
aasthadawar <aastha.dawar@tothenew.com>
2020-04-30 20:49:55 +0530

    session3

---
+++ Session3/Backend/utils/UserObject.js
@@ -0,0 +1,30 @@
+const studentsObject = {
+  students: [
+    {
+      firstname: 'aastha',
+      lastname: 'dawar',
+      branch: 'MEAN',
+      created_on: 'Thu Apr 30 2020 10:49:35',
+    },
+    {
+      firstname: 'latika',
+      lastname: 'malhotra',
+      branch: 'feen',
+      created_on: 'Thu Apr 30 2020 11:49:35',
+    },
+    {
+      firstname: 'megha',
+      lastname: 'prajapati',
+      branch: 'feen',
+      created_on: 'Thu Apr 30 2020 12:49:35',
+    },
+    {
+      firstname: 'akash',
+      lastname: 'aggarwal',
+      branch: 'java',
+      created_on: 'Thu Apr 30 2020 13:49:35',
+    },
+  ],
+};
+
+module.exports = studentsObject.students;
