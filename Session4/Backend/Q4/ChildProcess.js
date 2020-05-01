c4a03894673d6abbd306b7bf10d7a1f261babda6
aasthadawar <aastha.dawar@tothenew.com>
2020-05-02 04:24:42 +0530

    update child process

---
+++ Session4/Backend/Q4/ChildProcess.js
@@ -0,0 +1,8 @@
+const {execFile} = require('child_process');
+
+const childProcess=execFile('node',['--version'],(err,stdout,stderr)=>{
+    if(err){
+        console.log('error occured ::::',err);
+    }
+    console.log('output is ::::',stdout);
+})
